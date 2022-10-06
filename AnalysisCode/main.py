6d3a8e669a63e61da88fb0e956852acf5c961a96
Dhruvish-Patel <dhruvishpatel02@gmail.com>
2022-10-06 18:15:47 -0400

    update main.py

--- AnalysisCode/main.py
+++ AnalysisCode/main.py
@@ -4,4 +4,5 @@ from readaudio import audioinput
 from speechtotext import speech_to_text_
 
 print(audioinput(r"../data/audiofiles/data_audio_agent_0002f70f7386445b.wav"))
+print("--")
 print('\nSpeech to text:',speech_to_text_(r"../data/chunks"))
