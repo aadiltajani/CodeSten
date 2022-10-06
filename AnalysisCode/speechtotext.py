9a2ad5e7fd4bbb0d7b3ce44b4114fa8c337bd58e
Aadil Tajani <aadil.tajani@agami-tech.com>
2022-10-06 12:43:03 -0400

    Added error handling as speechrecognizer gives error sometimes

--- AnalysisCode/speechtotext.py
+++ AnalysisCode/speechtotext.py
@@ -9,8 +9,12 @@ def speech_to_text_(path):
         with sr.AudioFile(f) as source:
             audio_listened = r.record(source)
             # try converting it to text
-            text = r.recognize_google(audio_listened, language='en-Us')
-    return text
+            try:
+                text = r.recognize_google(audio_listened, language='en-Us')
+                return text
+            except sr.UnknownValueError as e:  # handling error if speech recognizer can't work on it
+                print("Error:", str(e))
+                return 'Couldn\'t work on this file'
 
 
 script_dir = os.path.dirname(__file__)  # abs path of current script needed to read audio file
