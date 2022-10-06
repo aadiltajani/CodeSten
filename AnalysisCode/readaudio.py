8212dd1ed0b35cf1e22ada838177013d43c768c5
Aadil Tajani <aadil.tajani@agami-tech.com>
2022-10-06 15:25:21 -0400

    made changes for errors

--- AnalysisCode/readaudio.py
+++ AnalysisCode/readaudio.py
@@ -5,19 +5,16 @@ from pydub.utils import make_chunks
 script_dir = os.path.dirname(__file__)  # abs path of current script needed to read audio file
 
 
-
 def trim_audio_file(path, folder_name):
     myaudio = AudioSegment.from_file(path, "wav")
     chunk_length_ms = 2000  # pydub calculates in millisec
     chunks = make_chunks(myaudio, chunk_length_ms)  # Make chunk
-    
-    
+
     os.mkdir(folder_name)
     for i, chunk in enumerate(chunks):
         chunk_name = "{0}.wav".format(i)
         print("exporting", chunk_name)
         chunk.export(folder_name + "/" + chunk_name, format="wav")
-
 
 
 def audioinput(file):
@@ -30,4 +27,5 @@ def audioinput(file):
 
 
 print(audioinput(os.path.join(script_dir, r"../data/audiofiles/data_audio_agent_0002f70f7386445b.wav")))
-print(audioinput(os.path.join(script_dir, r"../data/audiofiles/data_audio_caller_0002f70f7386445b.wav")))
+trim_audio_file(os.path.join(script_dir, r"../data/audiofiles/data_audio_agent_0002f70f7386445b.wav"),
+                os.path.join(script_dir, r"../data/chunks"))
--- AnalysisCode/speechtotext.py
+++ AnalysisCode/speechtotext.py
@@ -5,17 +5,24 @@ r = sr.Recognizer()
 
 
 def speech_to_text_(path):
-    with open(path, "rb") as f:
-        with sr.AudioFile(f) as source:
-            audio_listened = r.record(source)
-            # try converting it to text
-            try:
-                text = r.recognize_google(audio_listened, language='en-Us')
-                return text
-            except sr.UnknownValueError as e:  # handling error if speech recognizer can't work on it
-                print("Error:", str(e))
-                return 'Couldn\'t work on this file'
+    finaltext = {}
+    arr = os.listdir(path)
+    for x in arr:
+        xpath = os.path.join(path, x)
+        with open(xpath, "rb") as f:
+            with sr.AudioFile(f) as source:
+                audio_listened = r.record(source)
+                # try converting it to text
+                try:
+                    text = r.recognize_google(audio_listened, language='en-Us')
+                except sr.UnknownValueError as e:  # handling error if speech recognizer can't work on it
+                    print("Error:", str(e))
+                    finaltext[x] = "Error occurred"
+                else:
+                    text = f"{text.capitalize()}. "
+                    finaltext[x] = text
+    return finaltext
 
 
 script_dir = os.path.dirname(__file__)  # abs path of current script needed to read audio file
-print(speech_to_text_(os.path.join(script_dir, r"../data/audiofiles/data_audio_agent_0002f70f7386445b.wav")))
+print(speech_to_text_(os.path.join(script_dir, r"..\data\audiofiles\data_audio_agent_0002f70f7386445b.wav")))
