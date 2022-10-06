32d33eb13d46b17758a7346124897a83f58b7a82
Aadil Tajani <aadil.tajani@agami-tech.com>
2022-10-06 19:19:48 -0400

    Added checks for no emotion detected

--- AnalysisCode/emotionanalysis.py
+++ AnalysisCode/emotionanalysis.py
@@ -10,16 +10,20 @@ nltk.download('omw-1.4')
 
 def emotion_detection(text):
     emotionval = te.get_emotion(text)
-    print(emotionval.get('Happy'))
-    print(emotionval.get('Sad'))
-    print(emotionval)
+    # print(emotionval.get('Happy'))
+    # print(emotionval.get('Sad'))
+    # print(emotionval)
 
+    # need to check if there is no emotion detected
+    if (emotionval.get('Happy') == 0) and (emotionval.get('Sad') == 0) and (
+            emotionval.get('Fear') == 0) and (emotionval.get('Angry') == 0) and (
+            (emotionval.get('Surprise') == 0)):
+        max_key = "No emotion detected"
+        print("yes")
+    else:
+        max_key = max(emotionval, key=emotionval.get)
 
-    
-    max_key = max(emotionval, key=emotionval.get)
-    emotion = {}
-    emotion['possible_emotions'] = emotionval
-    emotion['emotion'] = max_key
+    emotion = {'possible_emotions': emotionval, 'emotion': max_key}
     # print("emotion_detection")
     # print(emotion)
     return emotion
