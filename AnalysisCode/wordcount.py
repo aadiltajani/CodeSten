9031052568279a465a94e4a9eaaf1533a25f71ad
KaiserJC <70088493+KaiserJC@users.noreply.github.com>
2022-10-06 19:31:20 -0400

    updated wordcount.py

--- AnalysisCode/wordcount.py
+++ AnalysisCode/wordcount.py
@@ -1,12 +1,17 @@
-def wordcount(text_dic):
+from speechtotext import speech_to_text_
 
+
+### For trial just provided user manual data to check if working or not
+def wordcount(text_dic):
+    number_of_words_1 = 0
     countval = 0
     for i in text_dic.values():
-        number_of_words = len(i)
+        word_list = i.split()
+        number_of_words = len(word_list)
         countval += number_of_words
-
     print(countval)
     return countval
 
-text_dict = {1: 'This', 2: 'is', 3: 'Earth'}
-x = wordcount(text_dict)
+
+text_dic = speech_to_text_('data/chunks')
+print(wordcount(text_dic))
