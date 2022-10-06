fd5f3d962797ac70fc9e2773ba5ccf7c6b020913
KaiserJC <70088493+KaiserJC@users.noreply.github.com>
2022-10-06 19:14:13 -0400

    created wordcount.py

--- AnalysisCode/wordcount.py
+++ AnalysisCode/wordcount.py
@@ -1,1 +1,13 @@
-from speechtotext import speech_to_text_
+def wordcount(text_dic):
+
+    countval = 0
+    for i in text_dic.values():
+        number_of_words = len(i)
+        countval += number_of_words
+
+    print(countval)
+    return countval
+
+
+text_dict = {1: 'This', 2: 'is', 3: 'Earth'}
+x = wordcount(text_dict)
