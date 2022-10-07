591284fd2bade96d27ca5cef2deb097fe96ee8dd
kaustubh <kadeshpa@ncsu.edu>
2022-10-06 22:44:07 -0400

    adding custom keyword extractor

--- AnalysisCode/keywordextract.py
+++ AnalysisCode/keywordextract.py
@@ -15,3 +15,13 @@ def getkewords(text):
     deduplication_algo = 'seqm'
     windowSize = 1
     numOfKeywords = 35
+    custom_kw_extractor = yake.KeywordExtractor(
+        lan=language,
+        n=max_ngram_size,
+        dedupLim=deduplication_thresold,
+        dedupFunc=deduplication_algo,
+        windowsSize=windowSize,
+        top=numOfKeywords,
+        features=None,
+        stopwords=data)
+
