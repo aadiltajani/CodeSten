78a09e5b18dd14af3141aaaf1279c90f4c7ca2fe
KaiserJC <70088493+KaiserJC@users.noreply.github.com>
2022-10-06 20:58:23 -0400

    updated keywordextract.py

--- AnalysisCode/keywordextract.py
+++ AnalysisCode/keywordextract.py
@@ -6,3 +6,12 @@ from speechtotext import speech_to_text_
 with open("data\stopwordlist\stopwords_en.txt", "r") as file:
     data = file.read()
     data = data.lower().split('\n')
+
+# from Analysis import stopwords
+def getkewords(text):
+    language = "en"
+    max_ngram_size = 2
+    deduplication_thresold = 0.9
+    deduplication_algo = 'seqm'
+    windowSize = 1
+    numOfKeywords = 35
