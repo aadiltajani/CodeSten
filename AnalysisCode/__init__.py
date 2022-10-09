9b371483273ca4937b063cfe6349f30b3182533e
Aadil Tajani <61664466+aadiltajani@users.noreply.github.com>
2022-10-09 15:15:17 -0400

    Update __init__.py

--- AnalysisCode/__init__.py
+++ AnalysisCode/__init__.py
@@ -1,13 +1,10 @@
-r'''
-# This is the Auto Generated Document for our project CODESTEN
-You can go through the following modules:
 
-1) main: https://aadiltajani.github.io/CodeSten/main.html
-2) readaudio: https://aadiltajani.github.io/CodeSten/readaudio.html
-3) speechtotext: https://aadiltajani.github.io/CodeSten/speechtotext.html
-4) sentimentanalysis:
-    https://aadiltajani.github.io/CodeSten/sentimentanalysis.html
-5) emotionanalysis: https://aadiltajani.github.io/CodeSten/emotionanalysis.html
-6) keywordextract: https://aadiltajani.github.io/CodeSten/keywordextract.html
-7) wordcount: https://aadiltajani.github.io/CodeSten/wordcount.html
-'''
+ """ 
+ Python package `pdoc` provides types, functions, and a command-line 
+ interface for accessing public documentation of Python modules, and 
+ for presenting it in a user-friendly, industry-standard open format. 
+ It is best suited for small- to medium-sized projects with tidy, 
+ hierarchical APIs. 
+  
+ .. include:: ./documentation.md 
+ """ 
