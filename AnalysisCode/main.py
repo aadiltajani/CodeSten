f7a01ea9a2a6cc247815732c1f7836897e51bf21
Aadil Tajani <61664466+aadiltajani@users.noreply.github.com>
2022-10-08 21:39:53 -0400

    Update main.py

--- AnalysisCode/main.py
+++ AnalysisCode/main.py
@@ -1,12 +1,11 @@
-import sys
-
-from emotionanalysis import emotion_detection
-from readaudio import audioinput
-from speechtotext import speech_to_text
-from wordcount import wordcount
-from sentimentanalysis import sentiment_scores
-from keywordextract import getkewords
-sys.path.append('./AnalysisCode')
+# import sys
+from AnalysisCode.emotionanalysis import emotion_detection
+from AnalysisCode.readaudio import audioinput
+from AnalysisCode.speechtotext import speech_to_text
+from AnalysisCode.wordcount import wordcount
+from AnalysisCode.sentimentanalysis import sentiment_scores
+from AnalysisCode.keywordextract import getkewords
+# sys.path.append('./AnalysisCode')
 
 print(audioinput(r"../data/audiofiles/data_audio_agent_0002f70f7386445b.wav"))
 text_dic = speech_to_text(r'../data/chunks')
