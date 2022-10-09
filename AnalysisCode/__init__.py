26d6b04cae1603e022174aad00c52fc246bb7272
Aadil Tajani <61664466+aadiltajani@users.noreply.github.com>
2022-10-09 15:56:43 -0400

    Update __init__.py

--- AnalysisCode/__init__.py
+++ AnalysisCode/__init__.py
@@ -1,26 +1,27 @@
 """
 # This is the Auto Generated Document for our project CODESTEN
 You can go through the following modules:
+
 1) main:
-https://aadiltajani.github.io/CodeSten/main.html
+https://aadiltajani.github.io/CodeSten/AnalysisCode/main.html
 
 2) readaudio:
-https://aadiltajani.github.io/CodeSten/readaudio.html
+https://aadiltajani.github.io/CodeSten/AnalysisCode/readaudio.html
 
 3) speechtotext:
-https://aadiltajani.github.io/CodeSten/speechtotext.html
+https://aadiltajani.github.io/CodeSten/AnalysisCode/speechtotext.html
 
 4) sentimentanalysis:
-https://aadiltajani.github.io/CodeSten/sentimentanalysis.html
+https://aadiltajani.github.io/CodeSten/AnalysisCode/sentimentanalysis.html
 
 5) emotionanalysis:
-https://aadiltajani.github.io/CodeSten/emotionanalysis.html
+https://aadiltajani.github.io/CodeSten/AnalysisCode/emotionanalysis.html
 
 6) keywordextract:
-https://aadiltajani.github.io/CodeSten/keywordextract.html
+https://aadiltajani.github.io/CodeSten/AnalysisCode/keywordextract.html
 
 7) wordcount:
-https://aadiltajani.github.io/CodeSten/wordcount.html
+https://aadiltajani.github.io/CodeSten/AnalysisCode/wordcount.html
 
-.. include:: ./README.md
+.. include:: ../README.md
 """
